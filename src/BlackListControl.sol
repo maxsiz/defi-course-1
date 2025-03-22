8c49f75a94e054d811cc44b146e4292974be44fc
Max Siz <maxsizmobile@gmail.com>
2025-03-22 16:48:17 +0700

    FEAT new black list hgook

---
+++ src/BlackListControl.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {PoolKey} from "v4-core/src/types/PoolKey.sol";
+import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
+
+contract BlackListControl {
+	address immutable public poolOwner;
+
+	// NOTE: ---------------------------------------------------------
+    // state variables should typically be unique to a pool
+    // a single hook contract should be able to service multiple pools
+    // ---------------------------------------------------------------
+    mapping(PoolId => mapping(address => bool)) public blackList;
+    error SenderBlackListed(address sender);
+
+    constructor () {
+    	poolOwner = msg.sender;
+    }
+
+    function setBlackListStatusForAddress(PoolId _pool, address _sender, bool _status) external {
+        
+        require (msg.sender == poolOwner, "Only For Pool Owner");
+        require (_sender != address(0), "No Zero Address");
+        blackList[_pool][_sender] = _status;
+    }
+
+    function _checkBlackListed(PoolId _pool, address _sender) internal view {
+        if (blackList[_pool][_sender]) {
+            revert SenderBlackListed(_sender);	
+        }
+    	
+    }
+}
---
+++ src/BlackListHook.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {BaseHook} from "v4-periphery/src/utils/BaseHook.sol";
+
+import {Hooks} from "v4-core/src/libraries/Hooks.sol";
+import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
+import {PoolKey} from "v4-core/src/types/PoolKey.sol";
+import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
+import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
+import {BeforeSwapDelta, BeforeSwapDeltaLibrary} from "v4-core/src/types/BeforeSwapDelta.sol";
+import {BlackListControl} from "./BlackListControl.sol";
+
+contract BlackListHook is BaseHook, BlackListControl {
+    using PoolIdLibrary for PoolKey;
+
+    // NOTE: ---------------------------------------------------------
+    // state variables should typically be unique to a pool
+    // a single hook contract should be able to service multiple pools
+    // ---------------------------------------------------------------
+
+    constructor(IPoolManager _poolManager) BaseHook(_poolManager) {}
+
+    function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
+        return Hooks.Permissions({
+            beforeInitialize: false,
+            afterInitialize: false,
+            beforeAddLiquidity: false,
+            afterAddLiquidity: false,
+            beforeRemoveLiquidity: false,
+            afterRemoveLiquidity: false,
+            beforeSwap: true,
+            afterSwap: false,
+            beforeDonate: false,
+            afterDonate: false,
+            beforeSwapReturnDelta: false,
+            afterSwapReturnDelta: false,
+            afterAddLiquidityReturnDelta: false,
+            afterRemoveLiquidityReturnDelta: false
+        });
+    }
+
+    // -----------------------------------------------
+    // NOTE: see IHooks.sol for function documentation
+    // -----------------------------------------------
+
+    function _beforeSwap(address, PoolKey calldata key, IPoolManager.SwapParams calldata, bytes calldata hookParams)
+        internal
+        override
+        returns (bytes4, BeforeSwapDelta, uint24)
+    {
+        //beforeSwapCount[key.toId()]++;
+        address txSender = abi.decode(hookParams, (address)); 
+        _checkBlackListed(key.toId(), txSender);
+        return (BaseHook.beforeSwap.selector, BeforeSwapDeltaLibrary.ZERO_DELTA, 0);
+    }
+
+    
+}
